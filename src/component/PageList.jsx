d7a78b91ff4cd01c4035af39c39c858127f7305f
Mau <mauricio.sdjr@gmail.com>
2020-07-27 19:39:43 -0300

    work progress

--- src/component/PageList.jsx
+++ src/component/PageList.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import CartIcon from './CartIcon';
 import CategoriesList from './CategoriesList';
+import InputSearch from './InputSearch';
 
 
 class PageList extends React.Component {
@@ -11,6 +12,7 @@ class PageList extends React.Component {
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        <InputSearch />
         <Link to="/cart">
           <CartIcon />
         </Link>
